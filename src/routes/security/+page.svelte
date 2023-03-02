3291be2827f78e42bd85e126efd25b4430b96007
Kieran Gill <kieran.gill1997@gmail.com>
2023-03-01 22:50:20 -0500

    add security explanation

---
+++ src/routes/security/+page.svelte
@@ -0,0 +1,15 @@
+<h2>Security</h2>
+
+<p>All of your data is stored locally on your machine.</p>
+
+<p>I do not have access to your API token or chat history.</p>
+
+<div>
+	You can verify this using two methods:
+	<ol>
+		<li>Check network requests</li>
+		<li>Read the code</li>
+	</ol>
+</div>
+
+<a href="/">go back to chat</a>
